b0583d8ac2af89149f9c2c59ae29b61104865b06
Ed Randall <github@edrandall.uk>
2023-12-13 15:44:27 +0100

    Changes to outputs

--- output.tf
+++ output.tf
@@ -1,6 +1,11 @@
-output "control_plane_public_ips" {
+output "ssh_via_internet" {
   value       = "ssh ubuntu@${aws_instance.edr_wg_instance.public_ip} -o 'StrictHostKeyChecking = no' -i ${var.public_key_path}"
-  description = "The Public IP for the EC2 Instance"
+  description = "SSH command to connect to the ec2 instance via the internet"
+}
+
+output "ssh_via_vpn" {
+  value       = "ssh ubuntu@$192.168.0.100 -o 'StrictHostKeyChecking = no' -i ${var.public_key_path}"
+  description = "SSH command to connect to the ec2 instance via the VPN"
 }
 
 output "wg_endpoint" {
